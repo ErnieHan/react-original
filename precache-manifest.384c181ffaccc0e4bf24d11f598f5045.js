dab6bab8cc47eb09766115b8ab9c7e9ff06aacf0
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2019-10-15 14:45:14 +0800

    Updates

---
+++ precache-manifest.384c181ffaccc0e4bf24d11f598f5045.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935f149527bae4ff794abab31022f8c6",
+    "url": "/react-original/index.html"
+  },
+  {
+    "revision": "b8fc64b081174cc2914d",
+    "url": "/react-original/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a17c539dc24dce93192a",
+    "url": "/react-original/static/js/2.0016b55b.chunk.js"
+  },
+  {
+    "revision": "b8fc64b081174cc2914d",
+    "url": "/react-original/static/js/main.78b1d052.chunk.js"
+  },
+  {
+    "revision": "0a3b9054710b8d1d9089",
+    "url": "/react-original/static/js/runtime-main.6d9d2aec.js"
+  }
+]);
