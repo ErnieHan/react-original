8a3e215210ef66d01aca638ea7264f4aaafc7395
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2019-11-20 13:46:48 +0800

    [UPDATE] 更改postResponse使用res.sendStatus(200)

--- json-server/postResponse.js
+++ json-server/postResponse.js
@@ -1,23 +1,24 @@
 module.exports = function(req, res, next) {
   if (req.method === "POST" && req.url === "/addresses") {
     res.method = "GET";
+    res.sendStatus(200);
     console.log("Address............", req.url);
   }
   if (req.method === "GET" && req.url === "/login") {
-    res.statusCode = 201;
+    res.sendStatus(201);
     if (res.statusCode === 201) console.log("Signed In............", req.url);
     if (res.statusCode === 200) console.log("Not Login ............", req.url);
   }
   if (req.method === "POST" && req.url === "/myFav") {
-    res.method = "GET";
+    res.sendStatus(200);
     console.log(
       "Change POST to GET while sending myFav API............",
       req.url
     );
   }
   if (req.method === "POST" && req.url === "/tempOrder") {
-    res.method = "GET";
-    res.statusCode = 201;
+    // Force to send status equal 200
+    res.sendStatus(200);
     res.send({
       errors: [
         {
