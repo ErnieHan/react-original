7a09ef1d80f84af63c1fc19cb1890ba9aec91500
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2020-02-13 16:26:56 +0800

    模擬 POST 這支API時 resoponse 寫進回傳資料

--- __tests__/components/IsPass.test.js
+++ __tests__/components/IsPass.test.js
@@ -6,12 +6,16 @@ describe("TESTING_ISPASS_COMPONENT", () => {
   const component = shallow(<IsPass />);
 
   test("COMPONENT_DISPLAY_SUCCESSFUL", () => {
+    // 尋找 Style Components <Content />
     expect(component.find("Content")).toHaveLength(1);
+    // <Content /> 內容文字為 foo
     expect(component.find("Content").text()).toEqual("foo");
   });
 
   test("ONCHANGE_TEST", () => {
+    // <Input /> onChange 事件 傳遞e.target.value = ernie
     component.find("Input").simulate("change", { target: { value: "ernie" } });
+    // .prop("value") 判斷 value 值
     expect(component.find("Input").prop("value")).toEqual("ernie");
   });
 
